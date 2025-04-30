a7d5d0afc84e4555272bb2510c87ab1411a07f27
Santidonatoo <santinodonato1@gmail.com>
2025-04-30 12:12:12 -0300

    Hibernate Util, PersonaDao y cambios en Persona

--- Tp_OO2_Grupo5/src/datos/Persona.java
+++ Tp_OO2_Grupo5/src/datos/Persona.java
@@ -2,10 +2,67 @@ package datos;
 
 import java.time.LocalDate;
 
-public class Persona {
+public abstract class Persona {
 	int idPersona;
 	String nombre;
 	String apellido;
 	long dni;
 	LocalDate fechaDeNacimiento;
+	
+	public Persona() {}
+	
+	public Persona(String nombre, String apellido, long dni, LocalDate fechaDeNacimiento) {
+		super();
+		this.nombre = nombre;
+		this.apellido = apellido;
+		this.dni = dni;
+		this.fechaDeNacimiento = fechaDeNacimiento;
+	}
+
+	public int getIdPersona() {
+		return idPersona;
+	}
+
+	protected void setIdPersona(int idPersona) {
+		this.idPersona = idPersona;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+	public long getDni() {
+		return dni;
+	}
+
+	public void setDni(long dni) {
+		this.dni = dni;
+	}
+
+	public LocalDate getFechaDeNacimiento() {
+		return fechaDeNacimiento;
+	}
+
+	public void setFechaDeNacimiento(LocalDate fechaDeNacimiento) {
+		this.fechaDeNacimiento = fechaDeNacimiento;
+	}
+
+	@Override
+	public String toString() {
+		return "Persona [idPersona=" + idPersona + ", nombre=" + nombre + ", apellido=" + apellido + ", dni=" + dni
+				+ ", fechaDeNacimiento=" + fechaDeNacimiento + "]";
+	}
+	
 }
