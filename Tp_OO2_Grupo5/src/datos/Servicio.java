9787a3a61f8d5af3bc09657bc4c9c91dfb65c766
echav <echav@192.168.0.215>
2025-04-30 12:12:16 -0300

    ServicioDao

--- Tp_OO2_Grupo5/src/datos/Servicio.java
+++ Tp_OO2_Grupo5/src/datos/Servicio.java
@@ -3,4 +3,32 @@ package datos;
 public class Servicio {
 	int idServicio;
 	String nombre;
+	
+	public Servicio() {}
+	
+	public Servicio(int idServicio, String nombre) {
+		this.idServicio = idServicio;
+		this.nombre = nombre;
+	}
+
+
+	public int getIdServicio() {
+		return idServicio;
+	}
+	protected void setIdServicio(int idServicio) {
+		this.idServicio = idServicio;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	@Override
+	public String toString() {
+		return "Servicio [idServicio=" + idServicio + ", nombre=" + nombre + "]";
+	}
+	
+	
 }
